4b4d6dcf0c11c045e021ed81a3eb43eaa568dad8
Gabriel Reliquias <relicote@gmail.com>
2024-05-10 23:59:49 -0300

    att

--- botGoogle.py
+++ botGoogle.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import pandas as pd
-# import requests
+from selenium.webdriver.chrome.options import Options
 import time
+# import requests
 
 nomeEmpresa = []
 servico = []
@@ -15,3 +15,12 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
 
 driver = webdriver.Chrome()
+
+driver.get('https://www.google.com.br/maps')
+time.sleep(5)
+
+driver.find_element(By.XPATH,'//*[@id="searchboxinput"]').send_keys('mooca')
+driver.find_element(By.XPATH,'//*[@id="searchbox-searchbutton"]').click()
+
+
+time.sleep(15)
