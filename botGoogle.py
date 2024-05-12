c5ff0e8a8b7d30ecd4fe91bb59deffd89d1b57b0
Gabriel Reliquias <relicote@gmail.com>
2024-05-12 00:08:14 -0300

    Update botGoogle.py

--- botGoogle.py
+++ botGoogle.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 import time
 # import requests
 
-nomeEmpresa = []
+empresa = []
 servico = []
 telefone = []
 
@@ -17,18 +17,32 @@ chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
 driver = webdriver.Chrome()
 
 driver.get('https://www.google.com.br/maps')
-time.sleep(5)
+time.sleep(2)
 
 driver.find_element(By.XPATH,'//*[@id="searchboxinput"]').send_keys('comercios proximos')
 driver.find_element(By.XPATH,'//*[@id="searchbox-searchbutton"]').click()
 
-time.sleep(5)
+time.sleep(2)
 
-card = driver.find_element(By.XPATH,'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[1]/div[1]')
+card = driver.find_element(By.XPATH,'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[1]/div[1]/div[3]/div/a')
 
-for i in range(len(card)):
-    if i == 0 or i == 1:
-        continue
+y = 3
+
+for i in range(12):
+    # if i == 0 or i == 1:
+    #     continue
+
+
+    driver.find_element(By.XPATH, f'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[1]/div[1]/div[{y}]/div/a')
+
+    nomeEmpresa = driver.find_element(By.XPATH, '//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[1]/div[1]/div[3]/div/div[2]/div[4]/div[1]/div/div/div[2]/div[1]/div[2]').text
+   
+    y = y + 2
+
+    empresa.append(nomeEmpresa)
+
+    print(card)
+    print(empresa)
 
 
 time.sleep(15)
